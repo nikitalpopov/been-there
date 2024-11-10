0fd4fb918ecee54453622ddec28bda0f3e258d10
Nikita Popov <mailto.nikitapopov@gmail.com>
2024-11-10 11:08:31 +0100

    chore: standalone API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/header/header.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'been-there'
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { enableProdMode } from '@angular/core'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-import { AppModule } from './app/app.module'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { enableProdMode, importProvidersFrom } from '@angular/core'
+import { MatNativeDateModule } from '@angular/material/core'
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app/app-routing.module'
+import { AppComponent } from './app/app.component'
 import { environment } from './environments/environment'
 
 if (environment.production) {
   enableProdMode()
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err))
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule, BrowserModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(MatNativeDateModule),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err))
